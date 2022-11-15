d6aaa9758af6b21f1b31e6f1cfa538930d0407c3
victorpazb <victor.braga@ccc.ufcg.edu.br>
2022-11-14 21:52:17 -0300

    second commit

--- teste.py
+++ teste.py
@@ -4,34 +4,51 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Firefox()
 
-url = 'https://www.camara.leg.br/deputados/quem-sao/resultado?search=&partido=&uf=&legislatura=56&sexo=M'
 
-driver.get(url)
-assert 'Busca de Deputados na Câmara dos Deputados - Portal da Câmara dos Deputados' in driver.title
-
-
-
-parentElement = driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/main/div[5]/div/div/section/ul')
-                                                  
-                                                
-elementList = parentElement.find_elements(By.CLASS_NAME, 'lista-resultados__item')
-
-
-
-print(len(elementList)) ##len = 25 (deputados na primeira pagina)
-
-complement = '&pagina='
-pagina = 2
-while(pagina < 22): ##there is 22 pages for male congressman 
-    
-    driver.get(url + complement + str(pagina))
+def scraperDeutados(gender):
+    url = 'https://www.camara.leg.br/deputados/quem-sao/resultado?search=&partido=&uf=&legislatura=56&sexo='+gender
+    driver.get(url)
     assert 'Busca de Deputados na Câmara dos Deputados - Portal da Câmara dos Deputados' in driver.title
 
+
     parentElement = driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/main/div[5]/div/div/section/ul')
-    elementList += (parentElement.find_elements(By.CLASS_NAME, 'lista-resultados__item'))
+                                                                                                       
+    elementList = parentElement.find_elements(By.CLASS_NAME, 'lista-resultados__item')
+
+    complement = '&pagina='
+    genString = ""
     
-    pagina+=1
+    if(gender == "F"):
+        maxPages = 5
+        genString = ' DEPUTADAS'
+    else:
+        maxPages = 22
+        genString = ' DEPUTADOS'
+        
+    
+    
+    pagina = 2
+    while(pagina < maxPages): ##there is 22 pages for congressman or 4 pages for congresswoman
+        
+        driver.get(url + complement + str(pagina))
+        assert 'Busca de Deputados na Câmara dos Deputados - Portal da Câmara dos Deputados' in driver.title
+
+        parentElement = driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/main/div[5]/div/div/section/ul')
+        elementList += (parentElement.find_elements(By.CLASS_NAME, 'lista-resultados__item')) ##aqui temos a captura da lista de uma pagina, nesse ponto podemos passar um 
+                                                                                              ## loop nessa sublista e tirar o que queremos. e segue normalmente.
+                                                                                              ## fiz dessa forma apenas pra ver que esta pegando todos os deputados
+        
+        pagina+=1
+        
+    
+    
+    print('São ' + str(len(elementList)) + genString) ##aqui podemos colocar pra retornar a lista de deputado(a)s return elementList e daí pra frente usar o pandas
     
 
+        
+homens = 'M'    
+scraperDeutados(homens)
 
-print(len(elementList)) ##len = 50? (deputados)
+
+mulheres = 'F'
+scraperDeutados(mulheres)
