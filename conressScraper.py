f41ae0487d025008152a12fcfcff191d91015eec
victorpazb <victor.braga@ccc.ufcg.edu.br>
2022-11-23 20:02:56 -0300

    .csv ok

--- conressScraper.py
+++ conressScraper.py
@@ -3,70 +3,113 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 import pandas as pd
+import csv
 
 driver = webdriver.Chrome()
 
-dadosDeputados = []
+dadosDeputadosNome = []
+dadosDeputadosPresenca = []
+dadosDeputadosGenero = []
+dadosCotaParlamentar = []
+dadosCotaGabinete = []
+
 
 
 def scraperDeutados(gender):
 
-    def getEveryCongressman(updatedUrl, sizeOfList):
+    def getEveryCongressman(updatedUrl, sizeOfList, gender):
         index = 0
         while(index < sizeOfList):
 
+            
             driver.get(updatedUrl)
             assert 'Busca de Deputados na Câmara dos Deputados - Portal da Câmara dos Deputados' in driver.title
 
             index += 1
+            
 
             xpathSFullString = '/html/body/div[2]/div[1]/main/div[5]/div/div/section/ul/li[' + str(
                 index) + ']/div[1]/h3/a'
-
+            
             statusXPATH = driver.find_element(
                 By.XPATH, '/html/body/div[2]/div[1]/main/div[5]/div/div/section/ul/li[' + str(index) + ']/div[1]/h3/span').text
-
+            
             deputado = driver.find_element(By.XPATH, xpathSFullString)
 
             # se for ex deputado, a página dele n deve ser aberta, nem seu detalhamento.
             if(statusXPATH == 'Em exercício'):
                 # abre cada página individual de cada congressista
+                
                 driver.find_element(By.XPATH, xpathSFullString).click()
 
                 # CAPTURAR NOME E PRESENÇA
+                
                 nome = driver.find_element(
                     By.XPATH, '//*[@id="identificacao"]/div/div/div[3]/div/div/div[2]/div[1]/ul/li[1]').text
-                print(nome)
-                presenças = driver.find_element(
+                
+                nomeArray = nome.split(':')
+                justNome = nomeArray[1]
+
+                try:
+                    presencaString = driver.find_element( 
                     By.XPATH, '//*[@id="atuacao-section"]/div[2]/ul[2]/li[1]/dl/dd[1]').text
+                    
+                    presencaNumberArray = presencaString.split(' ')
+                    presencaNumber = presencaNumberArray[0]
+                    #adicionando nome e presença ao arquivo
+                    
 
-                dadosDeputados.append([nome, presenças])
+                    intPresenca = int(presencaNumber)
+                    
+                    dadosDeputadosNome.append(justNome)
+                    dadosDeputadosPresenca.append(intPresenca)
+                    dadosDeputadosGenero.append(gender)  
+                except:
 
-                # abre o detalhamento dele
-                driver.find_element(
-                    By.XPATH, '/html/body/div[2]/div[1]/main/div[3]/div/div/div[1]/div/section[5]/ul/li[1]/div[2]/a').click()
+                    dadosDeputadosNome.append(justNome)
+                    dadosDeputadosPresenca.append(None)
+                    dadosDeputadosGenero.append(gender)
 
-                # AQUI COLOCA O FOREACH DE TBODY
-                tabela = driver.find_element(By.TAG_NAME, 'tbody')
+                try:
+                    # abre o detalhamento dele
+                    driver.find_element(
+                        By.XPATH, '/html/body/div[2]/div[1]/main/div[3]/div/div/div[1]/div/section[5]/ul/li[1]/div[2]/a').click()
 
-                for gasto in tabela.find_elements(By.TAG_NAME, 'td')[1:]:
-                    dadosDeputados.append(gasto.text)
+                    totalCotaParlamentarString = driver.find_element(By.ID, 'totalFinalAgregado').text
+                    totalCotaParlamentarArray = totalCotaParlamentarString.split(" ")
+                    dadosCotaParlamentar.append(totalCotaParlamentarArray[1])
+                except:
+                    dadosCotaParlamentar.append(None)
 
-                # abre a página de gastos de gabinete
+                #sai da paginas de detalhes e volta pra pagina do deputado
                 driver.back()
-                driver.find_element(
-                    By.XPATH, '//*[@id="gastos-section"]/ul/li[2]/div[2]/a').click()
 
-                # AQUI COLOCA O FOREACH DE TBODY
-                tabela = driver.find_element(By.TAG_NAME, 'tbody')
+                
+                try:
+                    #abre o detalhamento do gasto de gabinete
+                    driver.find_element(
+                        By.XPATH, '//*[@id="gastos-section"]/ul/li[2]/div[2]/a').click()
+                    
+                    verbaString = driver.find_element(By.XPATH, 
+                    '/html/body/div[2]/div[1]/main/section/div/table/tbody/tr[1]/td[2]').text
 
-                indexGabinete = 1
+                    
+                    verbaString = verbaString.replace('.','')
+                    verbaString = verbaString.replace(',','')
+                    dadosCotaGabinete.append(int(verbaString))
 
-                for gastos in tabela.find_elements(By.TAG_NAME, 'tr'):
-                    dado = gastos.find_element(
-                        By.XPATH, '//*[@id="main-content"]/section/div/table/tbody/tr[' + str(indexGabinete) + ']/td[3]')
-                    dadosDeputados.append(dado.text)
-                    indexGabinete += 1
+                except:
+                    dadosCotaGabinete.append(None)
+                        
+
+            
+
+
+                    
+                           
+                
+
+               
 
     url = 'https://www.camara.leg.br/deputados/quem-sao/resultado?search=&partido=&uf=&legislatura=56&sexo='+gender
     driver.get(url)
@@ -77,17 +120,21 @@ def scraperDeutados(gender):
     elementList = parentElement.find_elements(
         By.CLASS_NAME, 'lista-resultados__item')
 
+    
     if(gender == "F"):
         maxPages = 5
     else:
         maxPages = 22
 
+    
     x = len(elementList)
-    getEveryCongressman(url, x)
 
+    getEveryCongressman(url, x, gender)
+    
     complement = '&pagina='
     genString = ""
     pagina = 22
+
     while(pagina < maxPages):  # there is 22 pages for congressman or 4 pages for congresswoman
 
         updatedUrl = url + complement + str(pagina)
@@ -101,20 +148,29 @@ def scraperDeutados(gender):
 
         y = len(elementList)
 
-        getEveryCongressman(updatedUrl, y)
+        getEveryCongressman(updatedUrl, y, gender)
 
         pagina += 1
 
-    # return arquivo.csv?
+    
 
 
 homens = 'M'
 scraperDeutados(homens)
 
 
-""" mulheres = 'F'
-scraperDeutados(mulheres) """
+#mulheres = 'F'
+#scraperDeutados(mulheres)
 
-excel = pd.DataFrame([dadosDeputados])
 
-excel.to_csv('dadosDeputados.csv', index=False)
+
+nomes = pd.Series(dadosDeputadosNome)
+gender = pd.Series(dadosDeputadosGenero)
+presenca = pd.Series(dadosDeputadosPresenca)
+cotaParlamentar = pd.Series(dadosCotaParlamentar)
+cotaGabinete = pd.Series(dadosCotaGabinete)
+
+df_deputados = pd.DataFrame({ 'Nome': nomes, 'Presenças no ano': presenca, 'Gênero': gender, 'Total cota parlamentar':  cotaParlamentar, 'Cota disponivel gabinete': cotaGabinete})
+
+df_deputados.to_csv('deputados.csv', index=False)
+
